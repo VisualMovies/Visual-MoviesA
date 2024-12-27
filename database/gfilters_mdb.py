5859917a400c41f42526f1814120c07420e85fd7
Himanshu-4444 <154578231+Himanshu-4444@users.noreply.github.com>
2024-12-27 16:44:39 +0530

    Update gfilters_mdb.py

--- database/gfilters_mdb.py
+++ database/gfilters_mdb.py
@@ -1,6 +1,5 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @VisualMovies1
+# Ask Doubt on telegram @sgrph
 
 
 import pymongo
