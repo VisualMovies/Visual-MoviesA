70ced21cbe8c4b8da4c76e08bb79da23123e9584
VisualMovies <VisualMovies3@gmail.com>
2025-07-26 00:59:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,8 +112,8 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', False))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'modijiurl.com')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '220fed6ec94775c0d048c17a541e276aeb2bfeea')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'dashboard.smallshorts.com')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '901cca899db6273e13ce8783a8dbace0e1c8b807')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/howtoopenlinkclipmate/10')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
@@ -124,9 +124,9 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'modijiurl.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '220fed6ec94775c0d048c17a541e276aeb2bfeea')
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'dashboard.smallshorts.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '901cca899db6273e13ce8783a8dbace0e1c8b807')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/howtoopenlinkclipmate/10') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
